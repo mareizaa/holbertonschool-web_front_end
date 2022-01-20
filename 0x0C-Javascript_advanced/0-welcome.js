1a6fcd956563c4d48f68e067621346710514f813
mareizaa <marce.3210@gmail.com>
2022-01-19 20:21:23 -0500

    task 0

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,12 @@
+function welcome(firstName, lastName) {
+
+    let fullName = firstName + ' ' + lastName;
+
+    function displayFullName() {
+
+        alert('Welcome ' + fullName + ' !');
+
+    }
+
+    displayFullName();
+}
